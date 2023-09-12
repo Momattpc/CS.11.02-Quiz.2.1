5ae50818c965829c730d9e6ad4849b56dab7f73a
moses <moseskplai@gmail.com>
2023-09-12 14:17:30 +0800

    Added four methods

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,35 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static int add (int a,int b) {
+        return (a+b);
+    }
+    public static String greeting (String name)  {
+        return ("Bonjour, " + name + "!");
+    }
+    public static int add (int a, int b, int c, int d) {
+        return (add(d, add(c, add(a, b))));
+    }
+    public static String printMe (String toBePrinted){
+        return (toBePrinted);
+        }
+
+    public static void main(String[] args) {
+    int resultOne = add(5, 67);
+        System.out.println(resultOne);
+    String resultTwo = (greeting("Mr Ewing I'm unsure if you want general or specific"));
+        System.out.println(resultTwo);
+    int resultThree = add(2,3,4,6);
+        System.out.println(resultThree);
+        System.out.println(printMe("I hope this works"));
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
